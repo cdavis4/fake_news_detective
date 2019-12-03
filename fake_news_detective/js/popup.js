ef5df24523557168c5ebb9f839c147920fd8ebcb
locksinclair <sinclala@oregonstate.edu>
2019-12-02 21:09:35 -0800

    re adding the cycling

--- fake_news_detective/js/popup.js
+++ fake_news_detective/js/popup.js
@@ -12,31 +12,7 @@ const imgList = ["images/real16.png", "images/unknown16.png", "images/fake16.png
 var imgIndex = 0;
 
 window.onload=function(){
-<<<<<<< HEAD
-  
-  
-  let button = document.getElementById('geturl');
-
-  button.addEventListener ("click", function() {
-      var textarea = document.createElement('textarea');
-      document.body.appendChild(textarea);
-      textarea.value = window.location.href;
-
-
-  });
-
-  document.getElementById('changeIcon').addEventListener("click", function(){
-
-    let i = imgList[imgIndex];
-    console.log(i);
-    chrome.browserAction.setIcon({path: imgList[imgIndex]});
-    imgIndex= (imgIndex+1)%imgList.length;
-  });
-  
-
-=======
   //let button = document.getElementById('geturl');
->>>>>>> master
 
   //https://stackoverflow.com/questions/18150774/get-all-keys-from-chrome-storage
       document.getElementById('getUserInfo').addEventListener("click", function(){
@@ -55,5 +31,12 @@ window.onload=function(){
         urlhtml.innerHTML = cururl.changeurl;
     
     });
-    });   
+    }); 
+    document.getElementById('changeIcon').addEventListener("click", function(){
+
+      let i = imgList[imgIndex];
+      console.log(i);
+      chrome.browserAction.setIcon({path: imgList[imgIndex]});
+      imgIndex= (imgIndex+1)%imgList.length;
+    });  
 }
