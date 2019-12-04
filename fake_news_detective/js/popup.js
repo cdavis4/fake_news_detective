f6efda73fa3e4441612dfcf3bde0e3ead5738fd5
locksinclair <sinclala@oregonstate.edu>
2019-12-03 20:29:29 -0800

    got it working!

--- fake_news_detective/js/popup.js
+++ fake_news_detective/js/popup.js
@@ -34,67 +34,26 @@ window.onload=function(){
     }); 
     document.getElementById('changeIcon').addEventListener("click", function(){
       chrome.storage.local.get('currentIcon', function(filePath){
-        console.log(filePath[Object.keys(filePath)[0]].image);
-        chrome.browserAction.setIcon({path: filePath[Object.keys(filePath)[0]].image});  
-      }); 
 
-      chrome.storage.local.get(imgList[imgIndex], function(messagePath){
-        let tempString= messagePath[Object.keys(messagePath)[0]].message;
+        console.log(filePath[Object.keys(filePath)[0]].image);
+        chrome.browserAction.setIcon({path: filePath[Object.keys(filePath)[0]].image}); 
+
+        let tempString= filePath[Object.keys(filePath)[0]].message;
         chrome.browserAction.setTitle({title:tempString});
-      });
+
+
+      }); 
 
       imgIndex= (imgIndex+1)%imgList.length;
 
-      chrome.storage.local.get("currentIcon", function(updatePath){
-        console.log(updatePath);
-      });
-
-      chrome.storage.local.set({"currentIcon":{"image":"images/unknown16.png", "message":"The articles status is unknown"}});
-
-      chrome.storage.local.get("currentIcon", function(updatePath){
-        console.log(updatePath);
-      });
-
-      /*chrome.storage.local.get("currentIcon", function(updatePath){
-        console.log(updatePath);
-        chrome.storage.local.get(updatePath[Object.keys(updatePath)[0]], function(secondPath){
-          
-          console.log(secondPath);
-          chrome.storage.local.get({secondPath[Object.keys(secondPath)[0]]:"images/unknown16.png"});
-          console.log(secondPath);
+      chrome.storage.local.get(imgList[imgIndex], function(newPath){
+        let tempMessage= newPath[Object.keys(newPath)[0]].message;
+        let tempIcon=newPath[Object.keys(newPath)[0]].image;
+        console.log(tempMessage);
+        console.log(tempIcon);
+        chrome.storage.local.set({"currentIcon": {"image":tempIcon,'message':tempMessage}}, function(results) {
+          console.log(results);
         });
-
-
       });
-      
-      /*
-      chrome.storage.local.get(imgList[imgIndex], function(updatePath){
-
-        let throwAway = "";
-        let tempValues = throwAway.concat("{\"image\":\"", updatePath[Object.keys(updatePath)[0]].image, "\", message:\"", updatePath[Object.keys(updatePath)[0]].message, "\"}");
-        let temp = JSON.parse(tempValues);
-        chrome.storage.local.set({"currentIcon": temp});
-        console.log(updatePath);
-
-        chrome.storage.local.get("currentIcon", function(secondPath){
-          console.log(secondPath);
-        });
-        /*function (){
-          console.log("Changed the current icon.")
-        });
-      });*/
-
-
-      //this acts as the driver that changes the local storage currentIcon in the same way the background threads would
-      /*chrome.storage.local.set({"currentIcon": imgList[imgIndex]},
-      function (){
-        console.log("Changed the current icon.")
-  
-      });*/
-     /* chrome.storage.local.set({"currentIcon": chrome.storage.local.get(imgList[imgIndex], function(filePath2){
-        return 
-
-      }),*/
-
     });  
 }
